145f2e84c46e22d09b76c25ba48825a90e3b19c4
X-olani <33022688+X-olani@users.noreply.github.com>
2018-01-24 14:52:53 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false });
+var mongoose = require('mongoose');
+var fs = require("fs");
+var Reviews = require('./model/reviews');
+ var path = require('path');
+
+//mongodb://<dbuser>:<dbpassword>@ds127065.mlab.com:27065/codespace
+var mongoDB ='mongodb://Xolani:yo48chess@ds127065.mlab.com:27065/codespace'
+
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(express.static(path.join(__dirname, '/styless')));
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" +"index.html" );
+})
+
+
+app.post('/qte', urlencodedParser, function (req, res) {
+
+
+  var ccc = new Reviews({
+    author:req.body.author,
+    quote:req.body.quote,
+    rate:req.body.rate,
+  });
+});
+
+  //  var newReview = new Reviews ({
+  //  author: 'Mike Q',
+//   quote: 'its okay',
+  // rate: 3
+//  });
+
+//  newReview.save(function(err) {
+  //   if (err) throw err;
+
+  //  console.log('User created!');
+   //});
+
+
+   app.get('/revie', function(req,res){
+ Reviews.find({}, function(err, quotes){
+   if (err) throw  err;
+   res.end(JSON.stringify(quotes))
+});
+
+
+})
+
+
+
+
+
+
+
+var server = app.listen(8082, function () {
+
+   var host = server.address().address
+   var port = server.address().port
+ console.log("Example app listening at http://%s:%s", host,port)
+})
